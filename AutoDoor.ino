bbcd3b277f1722635c127224874de298f3b33888
3stan-H <tristanhsiung@gmail.com>
2025-05-16 22:02:07 -0400

    Add files via upload

---
+++ AutoDoor.ino
@@ -0,0 +1,50 @@
+#include <Servo.h> // servo library
+Servo s1;
+int trigPin = 10;
+int echoPin = 9;
+long duration;
+int distanceCm, distanceInch;
+void setup()
+{
+
+Serial.begin(9600); 
+pinMode(trigPin, OUTPUT);
+pinMode(echoPin, INPUT);
+
+s1.attach(3);   // Servo Motor
+
+
+
+}
+void loop() 
+{
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+duration = pulseIn(echoPin, HIGH);
+distanceCm= duration*0.034/2;
+distanceInch = duration*0.0133/2;
+Serial.println("Distance: ");
+Serial.println(distanceCm);
+delay(50);
+
+if(distanceCm < 8.5)
+{
+
+ s1.write(90);
+ delay(1000);
+
+}
+
+else
+
+{
+   s1.write(280);
+    delay(10);
+
+ 
+}
+
+}
